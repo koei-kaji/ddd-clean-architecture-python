5f666767e854076cfc66bfcb824f2a5a1342a632
koei-kaji <koei.kaji@gmail.com>
2022-11-27 14:28:29 +0900

    refactor: create base models for domain

--- src/todo/domain/models/tasks/task.py
+++ src/todo/domain/models/tasks/task.py
@@ -1,19 +1,13 @@
-from pydantic import BaseModel
-
-from custom_pydantic.config import BaseConfig
-
+from ..base_model import EntityModel
 from .task_id import TaskId
 from .task_name import TaskName
 from .task_status import TaskStatus
 
 
-class Task(BaseModel):
+class Task(EntityModel):
     task_id: TaskId
     name: TaskName
     status: TaskStatus
-
-    class Config(BaseConfig):
-        ...
 
     def change_name(self, name: TaskName) -> None:
         self.name = name
